817f120d2eccf5af9720029a935ba534906c0c88
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-26 13:54:48 +0200

    Update mongoose connection string to use new URL format

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nha-viec', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nha-viec', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Kết nối đến cơ sở dữ liệu thành công!'))
   .catch(err => console.error('Lỗi kết nối đến cơ sở dữ liệu:', err));
 
